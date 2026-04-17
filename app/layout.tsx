89566b191036dffafd369b485d5b2c4364db8467
Babalwa01 <babalwambolekwa93@gmail.com>
2026-04-17 17:24:22 +0200

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Outfit, Ovo } from "next/font/google";
+import { Analytics } from '@vercel/analytics/next';
 import "./globals.css";
 
 const outfit = Outfit({
@@ -26,6 +27,7 @@ export default function RootLayout({
         className={`${outfit.className} ${ovo.className} antialiased font-outfit leading-8 overflow-x-hidden`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
